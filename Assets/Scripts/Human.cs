8b53d1a12fa598a8cfce6b182dc9ac6e0b30a1c0
Jake-Mason476839 <72437199+Jake-Mason476839@users.noreply.github.com>
2021-11-17 00:26:27 +0000

    Human throw

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        anim.SetBool("Throw", false);
+        anim.SetBool("Walk", false);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            anim.SetBool("Throw", true);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("Walk", true);
+        }
+
+    }
+}
